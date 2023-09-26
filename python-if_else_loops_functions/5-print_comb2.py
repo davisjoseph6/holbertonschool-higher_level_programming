b8628566027401ee2288be905210166fc77019b2
Davis <davisjoseph767@gmail.com>
2023-09-26 08:54:59 -0700

    reformatted with format specifier

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    print("{:02d},".format(i), end=" ")
+for i in range(100):
+    if i < 10:
+        print("0{:d}".format(i), end=", ")
+    else:
+        print("{:d}".format(i), end=", ")
