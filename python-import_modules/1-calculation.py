0b671565db30ccca4a48e0027a92126ad38316d6
Davis <davisjoseph767@gmail.com>
2023-09-29 05:54:33 -0700

    shortened the code

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python3
 
-from calculator_1 import add
+from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
-    a = 1
-    b = 2
+    a = 10
+    b = 5
     result = add(a, b)
     print("{} + {} = {}".format(a, b, result))
 
-from calculator_1 import sub
-if __name__ == "__main__":
     a = 10
     b = 5
     result = sub(a, b)
     print("{} - {} = {}".format(a, b, result))
 
-from calculator_1 import mul
-if __name__ == "__main__":
     a = 10
     b = 5
     result = mul(a, b)
     print("{} * {} = {}".format(a, b, result))
 
-from calculator_1 import div
-if __name__ == "__main__":
     a = 10
     b = 5
     result = div(a, b)
