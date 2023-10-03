7aaf2e6ff293711c5bc741670c5eef384ae33730
Davis <davisjoseph767@gmail.com>
2023-10-03 06:47:34 -0700

    Wrote in pycodestyle

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -6,7 +6,7 @@ def print_matrix_integer(matrix=[[]]):
 
     for row in matrix:
         for i, num in enumerate(row):
-            if i == len(row) -1:
+            if i == len(row) - 1:
                 print("{:d}".format(num), end="")
             else:
                 print("{:d} ".format(num), end="")
