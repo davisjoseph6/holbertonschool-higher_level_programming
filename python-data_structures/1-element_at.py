fe13680e2293e9d9170d7387f16a64cb1f2f3fe3
Davis <davisjoseph767@gmail.com>
2023-10-03 03:08:39 -0700

    Wrote a function that retrieves an element from a list.

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if idx < 0:
+    if idx < 0 or idx > (len(my_list)):
         return None
-    elif idx > 4:
-        return None
-    else:
-        return my_list[idx]
+    for i, element in enumerate(my_list):
+        if i == idx:
+            return idx + 1
