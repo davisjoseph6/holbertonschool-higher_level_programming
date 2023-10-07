af09024754ccbaa581e047fdddebccbac1ab8818
Davis <davisjoseph767@gmail.com>
2023-10-07 01:32:52 -0700

    Wrote a function that divides element by element 2 lists.

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -7,8 +7,9 @@ def list_division(my_list_1, my_list_2, list_length):
             num1 = my_list_1[i] if i < len(my_list_1) else 0
             num2 = my_list_2[i] if i < len(my_list_2) else None
 
-            if not isinstance(num1, (int, float)) or not isinstance(num2, (int, float)):
-                raise TypeError("wrong type")
+            if (not isinstance(num1, (int, float))) or \
+                    (not isinstance(num2, (int, float))):
+                            raise TypeError("wrong type")
 
             if num2 == 0:
                 raise ZeroDivisionError("division by 0")
