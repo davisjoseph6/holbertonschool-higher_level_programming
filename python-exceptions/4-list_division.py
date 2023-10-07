045e952f4f4569a6ffff52ddf955a1d9fc77824e
Davis <davisjoseph767@gmail.com>
2023-10-07 01:26:57 -0700

    Wrote a function that divides element by element 2 lists.

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -26,5 +26,7 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print("out of range")
             result.append(0)
+        finally:
+            pass
 
     return result
