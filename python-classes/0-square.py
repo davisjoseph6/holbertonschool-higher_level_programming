fd968f8e29c03018a0d8000a33553199750f4033
Davis <davisjoseph767@gmail.com>
2023-10-09 08:49:29 -0700

    added the requirements

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
+"""
+This module defines an empty Square class.
+"""
+
 class Square:
-    pass
+    """
+    This class represents a square.
+    """
+
+    def __init__(self):
+        """
+        Initializes an instance of the Square class.
+        """
+        pass
 
 if __name__ == "__main__":
-    pass
+    my_square = Square()
