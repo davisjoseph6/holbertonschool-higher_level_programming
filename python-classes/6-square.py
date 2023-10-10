1b7616cef1bced81dc4c2609c15d4cfe9daa55d1
Davis <davisjoseph767@gmail.com>
2023-10-10 12:35:34 -0700

    fixed pycodestyle

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -20,8 +20,7 @@ class Square:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
+        self.__size = value
 
     @property
     def position(self):
@@ -29,14 +28,10 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if (
-                not isinstance(value, tuple)
-                or len(value) != 2
-                or not all(isinstance(x, int) for x in value)
-                or not all(x >= 0 for x in value)):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        else:
-            self.__position = value
+        if not isinstance(value, tuple) or len(value) != 2 or \
+                not all(isinstance(i, int) and i >= 0 for i in value):
+                    raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
     def area(self):
         return self.__size ** 2
