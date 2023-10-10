8ad42c5414c6e678fdd721cfb6871411add2e375
Davis <davisjoseph767@gmail.com>
2023-10-10 09:24:24 -0700

    added space

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -16,9 +16,10 @@ class Square:
         """Setter method to set the size attribute."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        else:
+            self.__size = value
 
     def area(self):
         """Calculate and return the area of the square."""
