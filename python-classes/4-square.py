8d975154d27022c43932df14d410db91939df7bf
Davis <davisjoseph767@gmail.com>
2023-10-10 09:27:02 -0700

    added space

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -3,17 +3,14 @@
 class Square:
 
     def __init__(self, size=0):
-        """Initialize a Square instance with an optional size."""
         self.__size = size
 
     @property
     def size(self):
-        """Getter method to retrieve the size attribute."""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Setter method to set the size attribute."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -22,5 +19,4 @@ class Square:
             self.__size = value
 
     def area(self):
-        """Calculate and return the area of the square."""
         return self.__size ** 2
