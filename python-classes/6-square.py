f8dba6398065aab950f56bb85a4f39c8916d3b81
Davis <davisjoseph767@gmail.com>
2023-10-10 12:24:03 -0700

    fixed pycodestyle

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -33,8 +33,7 @@ class Square:
                 not isinstance(value, tuple)
                 or len(value) != 2
                 or not all(isinstance(x, int) for x in value)
-                or not all(x >= 0 for x in value)
-            ):
+                or not all(x >= 0 for x in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
