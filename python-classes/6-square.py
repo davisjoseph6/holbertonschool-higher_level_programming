b2bae1f48c4c1e0e832403b9751572c346101dcb
Davis <davisjoseph767@gmail.com>
2023-10-11 05:54:41 -0700

    added position to the def print)

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -45,5 +45,7 @@ class Square:
         if self.__size == 0:
             print()
         else:
+            for _ in range(self.__position[1]):
+                print()
             for _ in range(self.__size):
-                print("#" * self.__size)
+                print(" " * self.__position[0] + "#" * self.__size)
