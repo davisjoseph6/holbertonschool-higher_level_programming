54b92af2c6675aff6704a7baa1b54f7ff6653609
Davis <davisjoseph767@gmail.com>
2023-10-12 10:16:41 -0700

    fixed errors

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+"""define function add rwo integers"""
 def add_integer(a, b=98):
-
+    '''function that adds 2 integers'''
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-
     return int(a) + int(b)
