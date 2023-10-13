2436acaeb87b4b8ef62de903e665d8ded3a09614
Davis <davisjoseph767@gmail.com>
2023-10-13 05:09:11 -0700

    shortened the code

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -51,11 +51,3 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-
-
-if __name__ == "__main__":
-    my_rectangle = Rectangle(4, 2)
-    print(my_rectangle.__dict__)
-    my_rectangle.width = 3
-    my_rectangle.height = 10
-    print(my_rectangle.__dict__)
