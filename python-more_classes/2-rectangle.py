1bbbca4070ef57ecccad127dd3c36b8512ab332f
Davis <davisjoseph767@gmail.com>
2023-10-13 08:56:36 -0700

    added documentation

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+This module defines an rectangle class.
+"""
+
+
 class Rectangle:
+    """
+    This class represents a rectangle
+    """
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
