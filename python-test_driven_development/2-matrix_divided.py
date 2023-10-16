a5434517a3ca9f2e588576b09c2b36d6ef29676f
Davis <davisjoseph767@gmail.com>
2023-10-16 02:25:22 -0700

    wrote code for task 2 and 3

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,21 +1,36 @@
 #!/usr/bin/python3
+"""
+   This module contains a matrix_divided(matrix, div)
+   function witch divides all elements of a matrix,
+   if matrix contains a non integer or float element
+   a TypeError is raised. If rows are not of the same
+   size, a TypeError is raised. If div is 0, a ZeroDivisionError
+   is raised.
+"""
+
+
 def matrix_divided(matrix, div):
-    if not all(
-            isinstance(row, list) and all(
-                isinstance(num, (int, float)) for num in row
-                ) for row in matrix
-            ):
-        raise TypeError(
-                "matrix must be a matrix (list of lists) of integers/floats"
-                )
-    if len(set(len(row) for row in matrix)) > 1:
-        raise TypeError("Each row of the matrix must have the same size")
+    """ Divides a matrix and returns a new matrix """
+    if not isinstance(matrix, list):
+        raise TypeError("matrix must be a (list of lists) of integers/floats")
+    for row in matrix:
+        if not isinstance(row, list):
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        for element in row:
+            if not isinstance(element, (int, float)):
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
-
-    if div == 0:
+    elif div == 0:
         raise ZeroDivisionError("division by zero")
-
-    result = [[round(num / div, 2) for num in row] for row in matrix]
-
-    return result
+    new_matrix = []
+    size = len(matrix[0])
+    for row in matrix:
+        if len(row) != size:
+            raise TypeError("Each row of the matrix must have the same size")
+        new_row = []
+        for element in row:
+            new_element = round(element / div, 2)
+            new_row.append(new_element)
+        new_matrix.append(new_row)
+    return new_matrix
--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
+""" say_my_name prints back the parameters
+first_name followed by last_name
+last_name is defaulted to an empty string
+"""
+
 
 def say_my_name(first_name, last_name=""):
-    if not isinstance(first_name, str) or not isinstance(last_name, str):
-        raise TypeError("first_name must be a string or last_name must be a string")
+    """Write a function that prints My name is <first name> <last name>"""
+    if not isinstance(first_name, str):
+        raise TypeError('first_name must be a string')
+    if not isinstance(last_name, str):
+        raise TypeError('last_name must be a string')
     else:
-        full_name = f"My name is {first_name} {last_name}".strip()
-        print(full_name)
+        print("My name is {} {}".format(first_name, last_name))
