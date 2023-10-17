d61b0276e001a1cf24f0bdc4169d81e239aac848
Davis <davisjoseph767@gmail.com>
2023-10-17 08:43:50 -0700

    wrote the documentation

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
+""" a function that returns True if the object"""
+"""is an instance of a class that inherited (directly or indirectly)"""
+"""from the specified class ; otherwise False."""
 
 
 def inherits_from(obj, a_class):
-    obj_class = type(obj)
-
-    while obj_class is not None:
-        if obj_class is a_class:
-            return True
-        obj_class = obj_class.__base__  # Get the base class
-    return False
+    """inherits from"""
+    return issubclass(type(obj), a_class)
