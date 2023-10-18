6bd6b3e1c1b29b41a2a2967d94ed608a21d46cc4
Davis <davisjoseph767@gmail.com>
2023-10-18 03:56:10 -0700

    Wrote a class BaseGeometry (based on 6-base_geometry.py).

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""a class BaseGeometry (based on 5-base_geometry.py)."""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def area(self):
+        """Raises an exception with a message."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates value"""
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
