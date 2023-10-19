9a34cde2eab718727e5340dd87736bf1e946932c
Davis <davisjoseph767@gmail.com>
2023-10-19 06:30:32 -0700

    modifed the files

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -6,7 +6,7 @@ class BaseGeometry:
     """BaseGeometry class"""
 
     def area(self):
-        """Raises an exception with a message."""
+        """Raises an exception with a message"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
