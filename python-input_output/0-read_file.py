5c2bf3aba1a0263fd8126bcbade47e34b0967cfe
Davis <davisjoseph767@gmail.com>
2023-10-20 09:08:00 -0700

    Wrote a function that reads a text file (UTF8) and prints it to stdout

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def read_file(filename=""):
+    try:
+        with open(filename, 'r', encoding='utf-8') as file:
+            for line in file:
+                print(line, end='')
+    except FileNotFoundError:
+        pass
