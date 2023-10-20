f3f2dd559a3f482c2698acab8425a433ceb7dd53
Davis <davisjoseph767@gmail.com>
2023-10-20 12:58:24 -0700

    shortened the code

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -8,10 +8,4 @@ import json
 
 
 def from_json_string(my_str):
-    """from jason string"""
-    try:
-        obj = json.loads(my_str)
-        return obj
-    except json.JSONDecodeError as e:
-        raise json.JSONDecodeError("Expecting ':' delimiter: line 1 column 34 (char 33)", e.doc, e.pos)
-
+    return json.loads(my_str)
