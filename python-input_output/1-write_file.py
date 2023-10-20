b903ea116b5cee92c5fa2aa9fa3a6934cad0d13a
Davis <davisjoseph767@gmail.com>
2023-10-20 10:13:35 -0700

    fixed pycodestyle

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Defines a function that writes a string to file (UTF8) and returns the number of char"""
+"""A function that writes a string to file utf8 returns the number of char"""
 
 
 def write_file(filename="", text=""):
@@ -7,4 +7,3 @@ def write_file(filename="", text=""):
     with open(filename, 'w', encoding='utf-8') as file:
         nb_characters = file.write(text)
     return nb_characters
-
