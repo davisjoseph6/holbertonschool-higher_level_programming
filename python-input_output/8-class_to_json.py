8fab20675f5d06bd896bd45017ef56c223e9b3f0
Davis <davisjoseph767@gmail.com>
2023-10-23 06:08:37 -0700

    changed the code

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,22 +1,7 @@
 #!/usr/bin/python3
-"""
-a function that returns the dictionary description with
-simple data structure (list, dictionary, string, integer and boolean)
-"""
+""" function that returns the dictionary description"""
 
 
 def class_to_json(obj):
-    """
-    class to json
-    """
-    if isinstance(obj, object):
-        # Get all attributes of the object
-        obj_dict = {}
-        for attr_name in dir(obj):
-            attr = getattr(obj, attr_name)
-            # Check if the attribute is of a serializable type
-            if isinstance(attr, (int, str, bool, list, dict)):
-                obj_dict[attr_name] = attr
-        return obj_dict
-    else:
-        raise ValueError("Input is not an instance of a class")
+    """ def class json obj"""
+    return obj.__dict__
