e6397eb6363f94771f545e82ed88cc9b668490d5
Davis <davisjoseph767@gmail.com>
2023-10-23 06:34:10 -0700

    Write a class Student that defines a student by: (based on 10-student.py)

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -4,13 +4,13 @@
 
 class Student:
     """class define"""
-    
+
     def __init__(self, first_name, last_name, age):
         """define parametre"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    
+
     def to_json(self, attrs=None):
         """define attr select"""
         if attrs is None:
@@ -21,7 +21,7 @@ class Student:
                 if i in self.__dict__:
                     new_attr[i] = self.__dict__[i]
             return new_attr
-    
+
     def reload_from_json(self, json):
         """ reload from json """
         for elem, value in json.items():
