0676108b8517cc767f598e38b185d26047ae12e4
Davis <davisjoseph767@gmail.com>
2023-10-23 07:22:15 -0700

    changed the code

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ import all module """
+
+
 import sys
 save = __import__('5-save_to_json_file').save_to_json_file
 load = __import__('6-load_from_json_file').load_from_json_file
