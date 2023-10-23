04ce1d96704481578c9688cddbfc397de0083d83
Davis <davisjoseph767@gmail.com>
2023-10-23 06:12:17 -0700

    Wrote a class Student that defines a student by

---
+++ python-input_output/9-student.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" def class student """
+
+
+class Student:
+    """ write parametre student class """
+    def __init__(self, first_name, last_name, age):
+        """ def attribut """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    def to_json(self):
+        """def dict return """
+        return self.__dict__
