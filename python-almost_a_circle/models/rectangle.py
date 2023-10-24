7a4e8c7d1aeabcf255ba89a8520fca8611cfdc5f
Davis <davisjoseph767@gmail.com>
2023-10-24 09:10:11 -0700

    Write the class Rectangle that inherits from Base:

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,23 +1,16 @@
 #!/usr/bin/python3
 """ first class base """
-
-
 class Base:
     """ base class """
-
     __nb_objects = 0
-
     def __init__(self, id=None):
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
-
 class Rectangle(Base):
     """ child class """
-
     def __init__(self, width, height, x=0, y=0, id=None):
         """
         Constructor for the Rectangle class.
