53e44873bbd14460ce6393db6860093ad3344f82
Davis <davisjoseph767@gmail.com>
2023-10-24 07:18:14 -0700

    changed the order

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -8,8 +8,8 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """ Constructor """
-        super().__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
+        super().__init__(id)
