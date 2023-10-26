892d22347e633ba1799b56535b35200debdf12e4
Davis <davisjoseph767@gmail.com>
2023-10-26 06:37:55 -0700

    imported json

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -15,6 +15,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """
         Convert a list of dictionaries to a JSON string
