75fa0295ee03f6636a1cf3a18b13fd3cde0e9784
Davis <davisjoseph767@gmail.com>
2023-10-26 05:57:47 -0700

    sqaure

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,35 +1,45 @@
 #!/usr/bin/python3
-""" Square module """
+"""write a class"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """ Square class, inherits from Rectangle """
+    """def class"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """ Initialize a Square instance """
         super().__init__(size, size, x, y, id)
+        self.size = size
 
     def __str__(self):
-        """ Return a string representation of the Square instance """
+        """Square"""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
 
+    @property
+    def size(self):
+        return self.width
 
-if __name__ == "__main__":
-    # Testing the Square class
-    s1 = Square(5)
-    print(s1)
-    print(s1.area())
-    s1.display()
-    print("---")
+    @size.setter
+    def size(self, value):
+        """size"""
+        self.width = value
+        self.height = value
 
-    s2 = Square(2, 2)
-    print(s2)
-    print(s2.area())
-    s2.display()
-    print("---")
+    def update(self, *args, **kwargs):
+        """def update attribut"""
+        if args:
 
-    s3 = Square(3, 1, 3)
-    print(s3)
-    print(s3.area())
-    s3.display()
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+    def to_dictionary(self):
+        """dictionary"""
+        return {'id': self.id, 'x': self.x, 'size': self.size, 'y': self.y}
