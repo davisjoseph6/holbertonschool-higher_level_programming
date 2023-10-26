f188b507fdc654608a7ab731ce93691c4ede2cd1
Davis <davisjoseph767@gmail.com>
2023-10-26 04:38:35 -0700

    changed the unittests

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,21 +1,36 @@
 #!/usr/bin/python3
-"""Unittest for max_integer([..])
+"""
+unitest for max integer in a list
 """
 import unittest
-max_integer = __import__('6-max_integer').max_integer
+max_int = __import__('6-max_integer').max_integer
+
 
 class TestMaxInteger(unittest.TestCase):
-    def test_max_integer_regular_list(self):
-        result = max_integer([1, 2, 3, 4])
-        self.assertEqual(result, 4)
 
-    def test_max_integer_negative_values(self):
-        result = max_integer([1, -2, 3, -4])
-        self.assertEqual(result, 3)
+    """ checks codes """
+    def test_max_list(self):
+        """ common use """
+        self.assertEqual(max_int([1, 4, 2, 3]), 4)
+        """ only negative numbers """
+        self.assertEqual(max_int([-4, -1, -2, -3]), -1)
+        """ max first element of list """
+        self.assertEqual(max_int([4, 3, 2, 3]), 4)
+        """ max last element of list """
+        self.assertEqual(max_int([1, 4, 2, 10]), 10)
+        """ max middle element of list """
+        self.assertEqual(max_int([1, 4, 5, 3, 2]), 5)
+        """ list contains 1 element """
+        self.assertEqual(max_int([10]), 10)
 
-    def test_max_integer_empty_list(self):
-        result = max_integer([])
-        self.assertIsNone(result)
+    def test_empty_list(self):
+        """ empty list """
+        self.assertEqual(max_int([]), None)
+
+    def test_errors(self):
+        self.assertRaises(TypeError, max_int, ["titi", 2])
+        self.assertRaises(TypeError, max_int, [3, [1, 3]])
+
 
 if __name__ == '__main__':
     unittest.main()
