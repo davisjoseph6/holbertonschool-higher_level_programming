e489e521f334e4af2b86f4c3a0d0d3a964f35f25
Davis <davisjoseph767@gmail.com>
2023-10-27 06:55:40 -0700

    task 17

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -24,6 +24,12 @@ class Base:
             return "[]"
         return json.dumps(list_dictionaries)
 
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string is None or json_string == "":
+            return[]
+        return json.loads(json_string)
+
     @classmethod
     def save_to_file(cls, list_objs):
         """
