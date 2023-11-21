2289e9870ce71e7a20f80aa649bb5b9146e7e7d4
Davis <davisjoseph767@gmail.com>
2023-11-21 07:58:15 -0800

    wrote the code

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,15 +1,3 @@
--- File: 5-full_table.sql
+-- 5-full_table.sql
 
--- Check if the database exists
-USE information_schema;
-SELECT SCHEMA_NAME
-FROM SCHEMATA
-WHERE SCHEMA_NAME = 'hbtn_0c_0';
-
--- Switch to the specified database
-USE hbtn_0c_0;
-
--- Retrieve table information
-SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, IS_NULLABLE
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'first_table';
+SHOW CREATE TABLE first_table;
