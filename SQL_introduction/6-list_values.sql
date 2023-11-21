29d45ba5999a0865851012e63853da18674dada5
Davis <davisjoseph767@gmail.com>
2023-11-21 08:10:02 -0800

    created the files

---
+++ SQL_introduction/6-list_values.sql
@@ -0,0 +1,5 @@
+-- 6-list_values.sql
+
+USE hbtn_0c_0;
+
+SELECT * FROM first_table;
---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,6 @@
+-- 7-insert_value.sql
+
+USE hbtn_0c_0;
+
+INSERT INTO first_table (id, name)
+VALUES (89, 'Best School');
