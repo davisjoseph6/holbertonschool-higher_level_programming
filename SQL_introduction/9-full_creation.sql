c4559cb92a21056ff6a11e602d30eecabd981d44
Davis <davisjoseph767@gmail.com>
2023-11-22 06:00:21 -0800

    wrote the code

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,13 @@
+-- Check if the table exists, and create it if not
+CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
+
+- Insert records into the second _table
+INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES
+	(1, 'John', 10),
+	(2, 'Alex', 3),
+	(3, 'Bob', 14),
+	(4, 'George', 8);
