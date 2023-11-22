adfb7313d41e5c21e1ce31276f7b798d441d66c1
Davis <davisjoseph767@gmail.com>
2023-11-22 08:41:07 -0800

    wrote the code

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,8 +1,5 @@
 -- 1-create_user.sql
 
--- Create user_0d_1 if it does not exist
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_Od_1_pwd';
-
 -- Grant all privileges to user_0d_1
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 
