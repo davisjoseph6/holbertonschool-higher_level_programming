ba6dbb3e85fcc9620f6e9a984fe19ba3e7712d47
Davis <davisjoseph767@gmail.com>
2023-11-22 08:09:31 -0800

    wrote the code

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,17 +1,17 @@
--- Create user_0d_1 if not exists
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+# Create user_0d_1
+echo "CREATE USER 'user_0d_1'@'localhost';" | mysql -hlocalhost -uroot -p
 
--- Grant privileges to user_0d_1
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+# Grant privileges to user_0d_1
+echo "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, ALTER ON *.* TO 'user_0d_1'@'localhost';" | mysql -hlocalhost -uroot -p
 
--- Create user_0d_2 if not exists
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+# Create user_0d_2
+echo "CREATE USER 'user_0d_2'@'localhost';" | mysql -hlocalhost -uroot -p
 
--- Grant privileges to user_0d_2
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
+# Grant privileges to user_0d_2
+echo "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, ALTER ON *.* TO 'user_0d_2'@'localhost';" | mysql -hlocalhost -uroot -p
 
--- Display privileges for user_0d_1
+# List privileges for both users
+cat <<EOF | mysql -hlocalhost -uroot -p
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
--- Display privileges for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
+EOF
