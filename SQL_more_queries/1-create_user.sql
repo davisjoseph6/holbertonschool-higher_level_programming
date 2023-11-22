78e8d55a84f0ddd422ef607639d2051637801949
Davis <davisjoseph767@gmail.com>
2023-11-22 08:44:46 -0800

    wrote the code

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,7 +1,2 @@
--- 1-create_user.sql
-
 -- Grant all privileges to user_0d_1
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-
--- Flush privileges to apply the changes
-FLUSH PRIVILEGES;
