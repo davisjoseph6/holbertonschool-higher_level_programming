f6b92ac455e270da033e1930efb49b48b67bc8a3
Davis <davisjoseph767@gmail.com>
2023-11-24 06:36:01 -0800

    wrote the code

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,7 +1,7 @@
 -- Create the table cities
 CREATE TABLE IF NOT EXISTS cities (
 	id INT AUTO_INCREMENT PRIMARY KEY,
-	state_id INT NOT NULL,
+	state_id INT,
 	name VARCHAR(256),
 	FOREIGN KEY (state_id) REFERENCES states(id)
 	);
