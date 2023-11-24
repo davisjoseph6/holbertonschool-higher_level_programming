75ce383de53ee12c2e7b1e07fad100431c6d89a3
Davis <davisjoseph767@gmail.com>
2023-11-24 05:23:19 -0800

    wrote the code

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,16 +1,7 @@
 -- 4-never_empty.sql
 
-USE hbtn_0d_2;
-
 -- Create the id_not_null table if it does not exist
 CREATE TABLE IF NOT EXISTS `id_not_null` (
   `id` INT DEFAULT 1,
   `name` VARCHAR(256)
 );
-
--- Insert sample data
-INSERT INTO `id_not_null` (`id`, `name`) VALUES (89, 'Best School');
-
--- Display the contents of the id_not_null table
-SELECT * FROM `id_not_null`;
-
