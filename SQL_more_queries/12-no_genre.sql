07d3bfaa64734ff331b1e19a03d339d3e78d4796
Davis <davisjoseph767@gmail.com>
2023-11-24 13:15:20 -0800

    wrote the code

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,7 +1,7 @@
 -- 12-no_genre.sql
 
-SELECT tv_shows.title, tv_show_genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genre_id IS NULL
+WHERE tv_show_genres.genre_id IS NULL
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
