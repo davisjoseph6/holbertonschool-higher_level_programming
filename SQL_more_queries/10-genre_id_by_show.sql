7301d4bf454e0c4b9898e11c2b91f973643d82d7
Davis <davisjoseph767@gmail.com>
2023-11-24 08:44:11 -0800

    wrote the code

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,7 @@
 -- 10-genre_id_by_show.sql
 
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-ORDER BY tv_shows.title, tv_show_genres.genre_id;
+FROM hbtn_0d_tv_shows.tv_shows
+JOIN hbtn_0d_tv_show.tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+WHERE tv_show_genres.genre_id IS NOT NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
