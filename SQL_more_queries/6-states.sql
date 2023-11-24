ecf5afaf2f15e2b17584389e44f7ad3d07506971
Davis <davisjoseph767@gmail.com>
2023-11-24 06:22:08 -0800

    wrote the code

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,8 +1,8 @@
--- Create table if not exists
+-- Create database if not exists
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 -- Use the database
 USE hbtn_0d_usa;
 
 -- Create table if not exists
-CREATE TABLE IF NOT EXISTS states ('id' INT AUTO_INCREMENT PRIMARY KEY, 'name' VARCHAR(256));
+CREATE TABLE IF NOT EXISTS states (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256));
