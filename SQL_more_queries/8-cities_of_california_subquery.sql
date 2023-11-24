c95508edc14f79334145189e405b9bce61117e3b
Davis <davisjoseph767@gmail.com>
2023-11-24 07:58:22 -0800

    wrote the code

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- 8-cities_of_california_subquery.sql
 
-SELECT * FROM cities
-WHERE state_id = (SELECT id FROM states WHERE name = 'California')
-ORDER BY id;
+SELECT id, name FROM cities
+WHERE state_id = 1
+ORDER BY cities.id ASC;
