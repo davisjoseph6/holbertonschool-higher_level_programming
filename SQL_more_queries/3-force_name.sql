5bf0b9cbf0d2cbfa11813a3a904c781bce49a52f
Davis <davisjoseph767@gmail.com>
2023-11-24 04:12:35 -0800

    created files

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -4,7 +4,7 @@ USE hbtn_0d_2;
 
 -- Check if the table exists
 CREATE TABLE IF NOT EXISTS force_name (
-	id INT,
-	name VARCHAR(256) NOT NULL,
-	PRIMARY KEY (id)
+    id INT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
 );
