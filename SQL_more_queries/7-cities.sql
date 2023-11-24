dd9fbcf162402a4b4189f5839139ad0b76de724b
Davis <davisjoseph767@gmail.com>
2023-11-24 06:47:35 -0800

    wrote the code

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,3 +1,15 @@
+-- Create the database if it does not exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Use the hbtn_0d_usa;
+USE hbtn_0d_usa;
+
+-- Create the states table if it does not exist
+CREATE TABLE IF NOT EXISTS states (
+	id INT PRIMARY KEY,
+	-- Add other columns as needed
+);
+
 -- Create the table cities
 CREATE TABLE IF NOT EXISTS cities (
     id INT AUTO_INCREMENT PRIMARY KEY,
