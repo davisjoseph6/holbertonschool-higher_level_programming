8833acca0906a68007deb6269349aed35cd6c6a7
Davis <davisjoseph767@gmail.com>
2023-11-24 04:09:54 -0800

    created files

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,7 +1,7 @@
 -- 3-force_name.sql
 
 -- Check if the table exists
-CREATE TABLE IF NOT EXISTS froce_name (
+CREATE TABLE IF NOT EXISTS force_name (
 	id INT,
 	name VARCHAR(256) NOT NULL,
 	PRIMARY KEY (id)
