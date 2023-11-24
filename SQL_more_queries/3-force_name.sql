b87064cfc536ec7aef6eec6379ef79632840d600
Davis <davisjoseph767@gmail.com>
2023-11-24 04:25:13 -0800

    wrote the code

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -3,4 +3,4 @@
 USE hbtn_0d_2;
 
 -- Check if the table exists before creating it
-CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL, PRIMARY KEY (id));
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256));
