0e8a856f7e93415847ab7a408e577784320bcab0
Davis <davisjoseph767@gmail.com>
2023-11-24 04:37:09 -0800

    wrote the code

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,2 +1,7 @@
--- Write a script that creates the table force_name on your MySQL server.
-CREATE TABLE IF NOT EXISTS `force_name`(`id` INT, `name` VARCHAR(256));
+-- 3-force_name.sql
+
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
