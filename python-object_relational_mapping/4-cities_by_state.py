3fd03707186f93e9866aca196a2a67728b1fc994
Davis <davisjoseph767@gmail.com>
2023-11-28 10:56:17 -0800

    wrote the code

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-""" script that lists all cities from the database hbtn_0e_4_usa  """
+"""Write a script that lists all cities from the database hbtn_0e_4_usa"""
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
 
-import MySQLdb
-import sys
+    if len(sys.argv) != 4:
+        sys.exit('Use: 4-cities_by_state.py<mysql username> <mysql password>'
+                 ' <database name>')
 
-
-if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                           passwd=sys.argv[2], db=sys.argv[3])
-    with conn.cursor() as cur:
-        cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
-                JOIN states ON cities.state_id = states.id\
-                ORDER BY cities.id ASC")
-
-        for row in cur.fetchall():
-            print(row)
-
+    conn = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset='utf8')
+    cur = conn.cursor()
+    cur.execute("SELECT ct.id, ct.name, st.name\
+                    FROM cities as ct\
+                    INNER JOIN states as st\
+                    ON ct.state_id = st.id ORDER BY id")
+    query_rows = cur.fetchall()
+    for city in query_rows:
+        print(city)
+    cur.close()
     conn.close()
