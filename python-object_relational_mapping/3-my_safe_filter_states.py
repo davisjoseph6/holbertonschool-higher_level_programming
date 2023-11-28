8ee954f17d0dbd60f40643f8c2f6a83f2c788bd9
Davis <davisjoseph767@gmail.com>
2023-11-28 10:37:10 -0800

    wrote the code

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,35 +1,18 @@
 #!/usr/bin/python3
-import sys
+""" script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument """
+
 import MySQLdb
+import sys
+
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: {} <username> <password> <database> <state>".format(sys.argv[0]))
-        sys.exit(1)
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3])
+    with conn.cursor() as cur:
+        cur.execute("SELECT * FROM states  ORDER BY id ASC")
+        for row in cur.fetchall():
+            if row[1] == sys.argv[4]:
+                print(row)
 
-    username, password, database, state = sys.argv[1:]
-
-    try:
-        # Connect to MySQL server
-        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
-        cursor = db.cursor()
-
-        # Execute a safe SQL query using parameterized query
-        query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-        cursor.execute(query, (state,))
-
-        # Fetch all the rows
-        rows = cursor.fetchall()
-
-        # Display the results
-        for row in rows:
-            print(row)
-
-    except MySQLdb.Error as e:
-        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
-
-    finally:
-        # Close the database connection
-        if 'db' in locals() and db:
-            db.close()
-
+    conn.close()
